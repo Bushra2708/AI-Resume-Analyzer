bd651645aa0517007c8c553758836ca10854d1d4
Bushra2708 <saifuddinbushra319@gmail.com>
2026-06-08 00:26:38 +0530

    done

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const API = axios.create({
   baseURL:
     import.meta.env.VITE_API_BASE_URL ||
-    "http://127.0.0.1:8000",
+    "https://ai-resume-analyzer-73jb.onrender.com",
 });
 
 export default API;
